1d595918d0422d7f6f336a07a9389c746b123130
Igor Sergiienko <isergiienko@evolutiongaming.com>
2019-03-05 16:19:24 +0200

    Init demo

---
+++ precache-manifest.bb0339310e412e4be4ca541a7c0157af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e36edec7f0099c81cc135fe93368b711",
+    "url": "/static/media/logo.e36edec7.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "dcc01f4f0939e53340d3",
+    "url": "/static/js/main.dcc01f4f.chunk.js"
+  },
+  {
+    "revision": "88a23591619c004d8fad",
+    "url": "/static/js/2.88a23591.chunk.js"
+  },
+  {
+    "revision": "dcc01f4f0939e53340d3",
+    "url": "/static/css/main.316601a1.chunk.css"
+  },
+  {
+    "revision": "88a23591619c004d8fad",
+    "url": "/static/css/2.4c28e7c9.chunk.css"
+  },
+  {
+    "revision": "afb43197cdc8dc95c007ab52ecae4f7a",
+    "url": "/index.html"
+  }
+];
